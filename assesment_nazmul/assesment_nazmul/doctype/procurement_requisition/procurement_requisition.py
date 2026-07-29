4a39d9a119a9282a5b54d9bc85055babc9cda5bb
Nazmul Hossain <nazmulhossain664@gmail.com>
2026-07-30 04:47:07 +0600

    feat: enhance procurement requisition doctype with auto-populated employee details and default item quantities

--- assesment_nazmul/assesment_nazmul/doctype/procurement_requisition/procurement_requisition.py
+++ assesment_nazmul/assesment_nazmul/doctype/procurement_requisition/procurement_requisition.py
@@ -1,9 +1,18 @@
 # Copyright (c) 2026, Nazmul Hossain and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
-
 class ProcurementRequisition(Document):
-	pass
+	def before_validate(self):
+		## set Requisition No if not set by user
+		if not self.requisition_no:
+			self.requisition_no = self.name
+
+		## set requested by Employee
+		employee = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, ["name"])
+		if employee:
+			self.requested_by = employee
+			
+			
